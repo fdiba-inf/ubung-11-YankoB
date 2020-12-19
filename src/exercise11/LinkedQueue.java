2c602685566e555904964e7c71e50c87f31ee2ab
YankoB <yankoo_bobev@abv.bg>
2020-12-19 01:58:44 +0000

    ubung11

--- src/exercise11/LinkedQueue.java
+++ src/exercise11/LinkedQueue.java
@@ -5,16 +5,28 @@ public class LinkedQueue {
     private Node backNode;
 
     public boolean isEmpty() {
-        // Check if queue has no elements
-        return false;
+        return frontNode == null;
     }
 
     public void offer(String data) {
-        // Offer element to queue
+        Node newNode= new Node(data);
+        if (isEmpty()){
+            frontNode = newNode;
+            backNode = newNode;
+        }
+        else{
+            backNode.setNextNode(newNode);
+            backNode = newNode;
+        }
     }
 
     public String poll() {
-        // Poll element from queue
+        if(!isEmpty()){
+            String data = frontNode.getData();
+            frontNode = frontNode.getNextNode();
+            return data;
+        }
+
         return null;
     }
 
